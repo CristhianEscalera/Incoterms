ef82f21e4083b9e5c098f70b2a56611dd8387239
NicoleHerbas <141873893+NicoleHerbas@users.noreply.github.com>
2023-09-15 14:33:47 -0400

    Add files via upload

---
+++ SolucionesMedicasBilbaoWeb/Admin.Master.cs
@@ -0,0 +1,39 @@
+﻿using SolucionesMedicasBilbaoDAO.Implementacion;
+using SolucionesMedicasBilbaoDAO.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SolucionesMedicasBilbaoWeb
+{
+    public partial class Admin : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Response.Cache.SetCacheability(HttpCacheability.NoCache);
+                Response.Cache.SetNoStore();
+            }
+            if (SessionClass.SessionRole != "Administrador")
+            {
+                if (SessionClass.SessionRole == "Ingresos")
+                {
+                    Response.Redirect("Ingresos.aspx");
+                }
+                else if (SessionClass.SessionRole == "Empleado")
+                {
+                    Response.Redirect("Empleado.aspx");
+                }
+                else
+                {
+                    Response.Redirect("WebLogin.aspx");
+                }
+            }
+        }
+    }
+}
---
+++ SolucionesMedicasBilbaoWeb/WebNewSupplier.aspx.cs
@@ -0,0 +1,39 @@
+﻿using SolucionesMedicasBilbaoDAO.Implementacion;
+using SolucionesMedicasBilbaoDAO.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SolucionesMedicasBilbaoWeb
+{
+    public partial class WebNewSupplier : System.Web.UI.Page
+    {
+        Supplier t;
+        SupplierImpl implSupplier;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnInsertar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                t = new Supplier(txtNit.Text.Trim(), txtName.Text.Trim(), txtDireccion.Text.Trim(), txtTelefono.Text.Trim(), txtSitioWeb.Text.Trim());
+                implSupplier = new SupplierImpl();
+                int n = implSupplier.Insert(t);
+                if (n > 0)
+                {
+                    Response.Redirect("WebAdmSupplier.aspx");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
