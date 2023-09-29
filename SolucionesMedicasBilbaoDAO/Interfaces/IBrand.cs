4202c8679090c14e5d667cb58f79d7dea3658013
NicoleHerbas <141873893+NicoleHerbas@users.noreply.github.com>
2023-09-29 09:31:04 -0400

    Add files via upload

---
+++ SolucionesMedicasBilbaoDAO/Interfaces/IBrand.cs
@@ -0,0 +1,17 @@
+﻿using SolucionesMedicasBilbaoDAO.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolucionesMedicasBilbaoDAO.Interfaces
+{
+    internal interface IBrand:IBase<Brand>
+    {
+        Brand Get(Byte id);
+
+        DataTable CargarCombo();
+    }
+}
